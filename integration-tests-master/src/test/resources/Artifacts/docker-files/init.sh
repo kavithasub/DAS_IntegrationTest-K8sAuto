fa81fe4e847d3a2eec1f526d704d13c970b3799a
kavithasub <kavitha@wso2.com>
2017-07-20 14:45:53 +0530

    updated

--- integration-tests-master/src/test/resources/Artifacts/docker-files/init.sh
+++ integration-tests-master/src/test/resources/Artifacts/docker-files/init.sh
@@ -3,17 +3,20 @@
 # Docker init script, this is the Entry Point of the Docker image
 # ----- These variables are parsed as Environment variables through Kubernetes controller
 
-echo "das_home is : " ${das_home}
+#echo "das_home is : " ${das_home}
 #echo "das_test_repo is : " ${das_test_repo}
 #echo "das_test_repo_name is : " ${das_test_repo_name}
-echo "Deployment Pattern is : " ${pattern}
+#echo "Deployment Pattern is : " ${pattern}
 
-#echo "Creating the DAS Home"
-#mkdir -p ${das_home}
 
-cd ${das_home}
-#mkdir distribution
+#cat > /etc/profile.d/set_java_home.sh << EOF
+#export JAVA_HOME="/usr/local/java/jdk1.8.0_51"
+#export PATH="/usr/local/java/jdk1.8.0_51/bin:\$PATH"
+#EOF
+
 #cp ${das_home}/wso2das-4.0.0-SNAPSHOT /home/distribution
 
-cd wso2das-4.0.0-SNAPSHOT/bin
-sh worker.sh
+cd /opt/distribution
+unzip -q product-4.0.0-SNAPSHOT.zip
+cd /wso2das-4.0.0-SNAPSHOT/bin
+sh /opt/distribution/wso2das-4.0.0-SNAPSHOT/bin/worker.sh
